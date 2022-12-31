0cf9dadd68ba71b90ead68700f0ebc1a15007059
Aditya Tyagi <adityatyagiav1@gmail.com>
2022-12-31 21:56:31 +0530

    course file modified

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,34 +1,45 @@
 import React from 'react'
 
-
+export function Sections({value}){
+    const exercises = value.parts.map(part=>part.exercises)
+    const total=exercises.reduce((s,p)=>{ return s+p;})
+    return(
+        <div>
+            <h2>{value.name}</h2>
+            <table>
+                <tbody>
+                {value.parts.map(part=>{
+                
+                return(
+                    <tr key={part.id}>
+                        {console.log(part)}
+                        <th>{part.name}</th>
+                        <th>{part.exercises}</th>
+                    </tr>
+                )
+            })}
+                </tbody>
+            </table>
+            <p><b>Total of {total} exercises</b></p>
+        </div>
+        
+    )
+}
 
 export default function Course({course}) {
     console.log(course)
-    const exercises = course.parts.map(part=>part.exercises)
-    const total=exercises.reduce((s,p)=>{ return s+p;})
-        
-    
+ 
   return (
     <div>
-
-
-        <h1>{course.name}</h1>
-        <table>
-            <tbody>
-                {course.parts.map(part=>{
+        {course.map(component=>{
+            return(
+                <div key={component.id}>
+                    <Sections value={component}/>
+                </div>
                 
-                    return(
-                        <tr key={part.id}>
-                            {console.log(part)}
-                            <th>{part.name}</th>
-                            <th>{part.exercises}</th><div className="3"></div>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-        <p><b>Total of {total} exercises</b></p>
-        
+            )
+        })
+        }
     </div>
   )
 }
