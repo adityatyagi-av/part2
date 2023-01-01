9fac4f170585ce2f64a290b9f96a0294e393c82f
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-01 22:59:32 +0530

    Notes file setup

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,5 @@
-import React from 'react'
+// import { eventWrapper } from '@testing-library/user-event/dist/utils'
+import React,{useState} from 'react'
 
 const Note =({note})=>{
     return(
@@ -7,7 +8,28 @@ const Note =({note})=>{
 }
 
 export default function Notes(props) {
-    const notes = props.notes
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState(
+        'a new note'
+    )
+    const addNote =(event)=>{
+        event.preventDefault()
+        console.log('button clicked',event.target)
+        const noteObject ={
+            content: newNote,
+            date: new Date().toISOString,
+            important: Math.random() < 0.5,
+            id: notes.length +1
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+      
+    }
+
+    const handleNoteChange=(event)=>{
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
   return (
     <div>
         <h1>Notes</h1>
@@ -18,6 +40,11 @@ export default function Notes(props) {
             )})
             }
         </ul>
+        <form onSubmit={addNote}>
+            
+            <input value={newNote} onChange={handleNoteChange}/>
+            <button type='submit'>Save</button>
+        </form>
     </div>
   )
 }
